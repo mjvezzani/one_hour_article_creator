46539afea20792fd169e508bdc47da53828b2ff0
Michael Vezzani <mjvezzani@gmail.com>
2014-11-07 08:39:31 -0500

    Create functionality to be able to navigate to new and edit routes to create and update articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,48 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:notice] = "Article created successfully"
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully"
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :author)
+  end
+
 end
