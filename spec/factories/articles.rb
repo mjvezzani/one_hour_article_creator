b81baf6e894b610526b55f5db2570e6b4b46867e
Michael Vezzani <mjvezzani@gmail.com>
2014-11-07 15:03:46 -0500

    Add factories to test suite. Adjust article model tests to utilize article fa factory

---
+++ spec/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :article do
+    title "Of Mice And Men"
+    author "John Steinbeck"
+  end
+end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,27 +1,23 @@
 require 'rails_helper'
 
 describe Article do
-  before :each do
-    @article = Article.new(title: 'Of Mice And Men',
-                           author: 'John Steinbeck')
-  end
-  it 'is valid with title and author' do
-    expect(@article).to be_valid
+  it 'is has a valid factory' do
+    expect(build(:article)).to be_valid
   end
   it 'is invalid without title' do
-    @article.title = ''
-    expect(@article).to be_invalid
+    article = build(:article, title: nil)
+    expect(article).to be_invalid
   end
   it 'is invalid without author' do
-    @article.author = ''
-    expect(@article).to be_invalid
+    article = build(:article, author: nil)
+    expect(article).to be_invalid
   end
   it 'is invalid with a duplicate title' do
-    Article.create(title: 'Of Mice And Men',
-                   author: 'John Steinbeck')
-    expect(@article).to be_invalid
+    create(:article)
+    expect(build(:article)).to be_invalid
   end
   it 'is able to return a string of Title: by Author' do
-    expect(@article.title_and_author).to eq 'Of Mice And Men: by John Steinbeck'
+    article = build(:article)
+    expect(article.title_and_author).to eq 'Of Mice And Men: by John Steinbeck'
   end
 end
